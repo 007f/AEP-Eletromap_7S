e2079e862094f4e848441d86fa2b32445d61caae
007f <88677725+007f@users.noreply.github.com>
2026-06-08 20:30:03 -0300

    Delete eletromap directory

--- eletromap/src/main/java/br/com/api/eletromap/model/enums/Status.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.eletromap.model.enums;
-
-public enum Status {
-    NORMAL,
-    IDENTIFICADO,
-    EM_REPARO,
-    NORMALIZADO,
-    FALHA // Para representar queda crítica reportada pelo medidor
-}
